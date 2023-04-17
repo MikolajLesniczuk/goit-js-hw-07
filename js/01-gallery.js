cc527bc6728aa6d4154fe4f788c57cecd52a23c7
MikolajLesniczuk <119354331+MikolajLesniczuk@users.noreply.github.com>
2023-04-17 22:29:54 +0200

    wip

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const Gallery = document.querySelector('.gallery')
+
+
+
+const createGallery = () => {
+  
+    let newGallery = [];
+
+galleryItems.forEach((e ,i) =>{
+const {preview , original , description} = galleryItems[i]
+
+const div = document.createElement('div');
+const a = document.createElement('a')
+const img = document.createElement('img')
+
+div.classList.add('gallery__item')
+a.classList.add('gallery__link')
+img.classList.add('gallery__image')
+
+a.setAttribute('href' , original)
+img.setAttribute('src', preview)
+img.setAttribute('data-source',original)
+img.setAttribute('alt',description )
+
+div.append(a)
+a.append(img)
+
+newGallery.push(div)
+
+
+})
+Gallery.append(...newGallery)
+
+}
+Gallery.addEventListener("click", (event) => {
+    event.preventDefault();
+ 
+    const instance = basicLightbox.create(
+      `<img src="${event.target.dataset.source}">`
+      
+    );
+  
+    instance.show();
+  
+    const visible = basicLightbox.visible();
+    if (visible) {
+      document.addEventListener('keyup', ( {key} ) => {
+        if (key === 'Escape') {
+          instance.close();
+        }
+      });
+    }
+  });
+
+createGallery()
+
 
 console.log(galleryItems);
